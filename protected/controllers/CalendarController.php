ed6107796fea066ba5f2f406e1350538bd661e74
aks <isanybodyhere@gmail.com>
2011-08-28 15:18:59 +0400

    * супер-изменения!

---
+++ protected/controllers/CalendarController.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ *
+ */
+class CalendarController extends ElementController {
+	public function actionView($id = false) {
+		$data = array('id'=>$id);
+		parent::actionView($data);
+	}
+	
+	public function actionRender($id = false) {
+		$data = array('id'=>$id);
+		parent::actionRender($data);
+	}
+	
+	public function actionStatus($id = false) {
+		$data = array('id'=>$id);
+		parent::actionStatus($data);
+	}
+}
---
+++ protected/views/elements/calendar.php
@@ -0,0 +1,5 @@
+<div class="time">
+	22<span>:</span>22
+</div>
+<div class="date">чт, 7 апреля 1983</div>
+<div class="innershadow full"></div>
